f6b00ad2cfd4ea3159ba6ff7a4f68ccb67a5bcb8
jcprice12 <jcprice12@gmail.com>
2017-08-07 20:32:45 -0400

    getting used to firebase authentication

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,12 +1,50 @@
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCt-XPcbfE1R6eChVyXWvO-kV0IYS6eiCY",
-    authDomain: "rockpaperscissors-73d01.firebaseapp.com",
-    databaseURL: "https://rockpaperscissors-73d01.firebaseio.com",
-    projectId: "rockpaperscissors-73d01",
-    storageBucket: "rockpaperscissors-73d01.appspot.com",
-    messagingSenderId: "580909160554"
-  };
-  firebase.initializeApp(config);
+var config = {
+	apiKey: "AIzaSyCt-XPcbfE1R6eChVyXWvO-kV0IYS6eiCY",
+	authDomain: "rockpaperscissors-73d01.firebaseapp.com",
+	databaseURL: "https://rockpaperscissors-73d01.firebaseio.com",
+	projectId: "rockpaperscissors-73d01",
+	storageBucket: "rockpaperscissors-73d01.appspot.com",
+	messagingSenderId: "580909160554"
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
+var authorization = firebase.auth();
+
+
+$(document).ready(function(){
+	$("#signUpButton").on("click", function(){
+		const email = $("#emailInput").val();
+		const password = $("#passwordInput").val();
+		authorization.createUserWithEmailAndPassword(email, password).catch(function(error) {
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			console.log("Error Code: " + error.code);
+			console.log("Error Message: " + error.message);
+		});
+	});
+
+	$("#loginButton").on("click", function(){
+		const email = $("#emailInput").val();
+		const password = $("#passwordInput").val();
+		firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error){
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			console.log("Error Code: " + error.code);
+			console.log("Error Message: " + error.message);
+		});
+	});
+
+	$("#logOutButton").on("click", function(){
+		authorization.signOut();
+	});
+
+	authorization.onAuthStateChanged(function(myUser){
+		if(myUser){
+			console.log(myUser);
+		} else {
+			console.log("not logged in");
+		}
+	})
+});
